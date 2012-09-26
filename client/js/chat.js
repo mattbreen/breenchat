81373c8f85b154deedfb6560e319334177b56c56
Dan Breen <dan@danbreen.net>
2012-09-25 23:15:37 -0400

    Initial commit

---
+++ client/js/chat.js
@@ -0,0 +1,108 @@
+var ChatServer = function() {
+    this.socket = undefined;
+};
+
+ChatServer.prototype = {
+    send: function(params) {
+        this.socket.send(JSON.stringify(params));
+    },
+
+    login: function(handle) {
+        var me = this;
+        this.handle = handle;
+        this.socket = new WebSocket('ws://localhost:8080/');
+        this.socket.onopen = function () {
+            me.log("Chat connection opened");
+            me.send({'type': 'login', 'handle': handle});
+        };
+        this.socket.onmessage = function(event) {
+            message = $.parseJSON(event.data);
+            switch(message.type) {
+                case 'message':
+                    me.log(message.message, message.handle);
+                    break;
+                case 'user_joined':
+                    me.log(message.handle + " has joined the channel");
+                    me.add_user(message.id, message.handle);
+                    break;
+                case 'user_left':
+                    me.log(message.handle + " has left the channel");
+                    me.remove_user(message.id);
+                    break;
+                case 'userlist':
+                    $.each(message.users, function(idx, user) {
+                        me.add_user(user.id, user.handle);
+                    });
+                    break;
+            }
+        };
+        this.socket.onclose = function () {
+            me.log("Chat connection closed");
+        };
+    },
+
+    add_user: function(id, handle) {
+        var $el = $('<li id="user-'+id+'">'+handle+'</li>').hide();
+        $('#userlist').append($el);
+        $el.fadeIn(500, this.update_count);
+    },
+
+    remove_user: function(id) {
+        var $el = $('#userlist #user-'+id),
+            me = this;
+        $el.fadeOut(500, function() {
+            $(this).remove();
+            me.update_count();
+        });
+    },
+
+    update_count: function() {
+        $('#usercount').text($('#userlist li').length);
+    },
+
+    message: function(message) {
+        this.send({'type': 'message', 'message': message});
+    },
+
+    timestamp: function() {
+        var date = new Date(),
+            hrs = date.getHours(),
+            mins = date.getMinutes(),
+            secs = date.getSeconds();
+        return (hrs < 10 ? "0" : "") + hrs + ":" + (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
+    },
+
+    log: function(msg, user) {
+        var system = user == undefined;
+        $('#log').append('<div class="message'+(system ? ' message-system' : '')+'"><span class="time">'+this.timestamp()+'</span>: '+ (system ? '' : '<span class="user">'+user+':</span> ') +msg+'</div>');
+        $('#log').scrollTop($('#log')[0].scrollHeight);
+    }
+};
+
+window.chat = new ChatServer();
+
+$(function() {
+
+	$('#handle-dlg').modal('show')
+		.on('shown', function() {
+			$('#handle').focus();
+			$('#handle-form').submit(function() {
+				$('#handle-dlg').modal('hide');
+				return false;
+			});
+		})
+		.on('hide', function() {
+			var handle = $('#handle').val();
+			window.chat.login(handle);
+			$('#chat-panel').fadeIn();
+			$('#chat').focus();
+			$('#my-handle').html(handle);
+		});
+
+	$('#chatform').submit(function() {
+        window.chat.message($('#chat').val());
+	    $('#chat').val('');
+	    return false;
+	});
+
+});
---
+++ server/server.py
@@ -0,0 +1,100 @@
+import simplejson
+from twisted.internet import protocol, reactor
+from txws import WebSocketFactory
+
+
+MESSAGE_TYPES = ('login', 'message')
+
+
+class Chatter(protocol.Protocol):
+    def __init__(self, factory, id):
+        self.factory = factory
+        self.id = id
+        self.handle = 'unknown'
+
+    def _build(self, type_, params=None):
+        if params is None:
+            params = {}
+        params.update({
+            'type': type_,
+        })
+        return simplejson.dumps(params)
+
+    def _write(self, message):
+        print "[%s,%d] OUT: %s" % (self.handle, self.id, message)
+        self.transport.write(message)
+
+    def reply(self, type_, params):
+        self._write(self._build(type_, params))
+
+    def broadcast(self, type_, params):
+        self.factory.broadcast(self._build(type_, params))
+
+    def error(self, message):
+        print "ERROR: %s" % message
+        self.reply('error', {'message': message})
+
+    def dataReceived(self, data):
+        print "[%s,%d]  IN: %s" % (self.handle, self.id, data)
+        try:
+            json = simplejson.loads(data)
+        except ValueError:
+            self.error('Invalid message sent')
+            return
+        type_ = json.get('type')
+        if not type_ or type_ not in MESSAGE_TYPES:
+            self.error('Invalid message type')
+        getattr(self, 'handle_%s' % type_)(json)
+
+    def handle_message(self, params):
+        self.broadcast('message', {
+            'handle': self.handle,
+            'message': params['message'],
+        })
+
+    def handle_login(self, params):
+        self.handle = params['handle']
+        self.reply('userlist', {
+            'users': [
+                {'id': u.id, 'handle': u.handle}
+                for u in self.factory.chatters
+                if u != self
+            ]
+        })
+        self.broadcast('user_joined', {
+            'handle': self.handle,
+            'id': self.id
+        })
+
+    def connectionLost(self, reason):
+        params = {
+            'handle': self.handle,
+            'id': self.id
+        }
+        self.factory.remove(self)
+        self.broadcast('user_left', params)
+
+
+class ChatterFactory(protocol.Factory):
+    chatters = []
+    next_id = 0
+
+    def buildProtocol(self, addr):
+        self.next_id += 1
+        chatter = Chatter(self, self.next_id)
+        self.chatters.append(chatter)
+        return chatter
+
+    def broadcast(self, message):
+        for chatter in self.chatters:
+            chatter._write(message)
+
+    def remove(self, chatter):
+        self.chatters.remove(chatter)
+
+
+print "Starting reactor..."
+reactor.listenTCP(8080, WebSocketFactory(ChatterFactory()))
+print "Listening..."
+reactor.run()
+
